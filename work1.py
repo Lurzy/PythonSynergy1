30eeb285269a12beac68a91fa3741b50d8c9e7da
Lurzy <purpleswtf@gmail.com>
2024-04-08 21:40:12 +0300

    Добавлена первая задача, обновлен gitignore и readme

---
+++ work1.py
@@ -0,0 +1,8 @@
+print("Введите вид питомца: ")
+petKind = input()
+print("Введите возраст питомца: ")
+petAge = input()
+print("Введите кличку питомца: ")
+petName = input()
+
+print("Это ", petKind, 'По кличке "',petName,'". Возраст:  ', petAge, "года." )
